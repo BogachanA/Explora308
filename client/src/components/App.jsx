55ef124c4a17404f3b23b80982c2993212e0e484
BogachanA <bogacbugra@gmail.com>
2021-11-10 18:44:36 +0300

    First one

---
+++ client/src/components/App.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  return (
+    <div>
+      <div className="main-screen">
+        <h1 className="header">
+          <strong>Welcome to Explora!</strong>
+        </h1>
+        <br></br>
+        <div className = "container">
+          <div className="login">
+          <h2>Already have an account</h2>
+          <br></br>
+            <form>
+              <input
+                onChange={handleChange}
+                name="username"
+                value={contact.username}
+                placeholder="Username"
+              />
+              <input
+                onChange={handleChange}
+                name="password"
+                value={contact.password}
+                placeholder="Password"
+              />
+              <button className ="button">Login</button>
+            </form>   
+          </div>
+          <div className="register">
+            <h2> <b> Not a user? </b> </h2>
+            <br></br>
+            <form action="/api/users/register" method="POST">
+              <input
+                onChange={handleChange}
+                name="name"
+                value={contact.name}
+                placeholder="Name"
+              />
+              <input
+                onChange={handleChange}
+                name="email"
+                value={contact.email}
+                placeholder="Email"
+              />
+              <input
+                  onChange={handleChange}
+                  name="password"
+                  value={contact.password}
+                  placeholder="Password"
+              />
+              <input
+              onChange={handleChange}
+              name="password2"
+              value={contact.password2}
+              placeholder="Confirm Password"
+            />
+              <button type="submit">Register</button>
+            </form> 
+          </div>  
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://exploraadmin:cs308cs308@cluster0.zl4hb.mongodb.net/exploradata?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
