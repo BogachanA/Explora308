c8dc9df6095f2f4aa4da295c3a260b66fcab8b74
Serra Yilmaz <serrayilmaz@sabanciuniv.edu>
2021-11-22 15:44:24 +0300

    Add files via upload

---
+++ client/src/components/preferences.js
@@ -0,0 +1,45 @@
+const preferences = [
+    {
+        value: 'cultural and heritage tourism',
+        label: 'Cultural and Heritage Tourism'
+    }, {
+        value: 'shopping',
+        label: 'Shopping'
+    }, {
+        value: 'hiking',
+        label: 'Hiking'
+    }, {
+        value: 'museum',
+        label: 'Museum'
+    }, {
+        value: 'food tourism',
+        label: 'Food Tourism'
+    }, {
+        value: 'architectural beauty',
+        label: 'Architectural Beauty'
+    }, {
+        value: 'aquarium',
+        label: 'Aquarium'
+    }, {
+        value: 'beach-walking area',
+        label: 'Beach-Walking Area'
+    }, {
+        value: 'garden',
+        label: 'Garden'
+    }, {
+        value: 'landmark',
+        label: 'Landmark'
+    }, {
+        value: 'park',
+        label: 'Park'
+    }, {
+        value: 'observatory',
+        label: 'Observatory'
+    }, {
+        value: 'nightlife',
+        label: 'Nightlife'
+    }
+
+];
+
+export default preferences;
