cfebb331defccd43b26702a5930edcd7d069cc8e
Serra Yilmaz <serrayilmaz@sabanciuniv.edu>
2021-11-29 13:29:24 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import App from "./components/App";
 import Landing from "./components/Landing";
 import Profile from "./components/Profile";
 import Forms from "./components/Forms";
+import Attractions from "./components/Attractions";
 
 ReactDOM.render(<Profile />, document.getElementById("root"));
