01d755325a73794df2dd612888d835201c0600d1
Serra Yilmaz <serrayilmaz@sabanciuniv.edu>
2021-12-12 18:22:48 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,5 +6,7 @@ import Attractions from "./components/Attractions";
 import Profile from "./components/Profile";
 import Forms from "./components/Forms";
 import Attractions from "./components/Attractions";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 ReactDOM.render(<Attractions />, document.getElementById("root"));
