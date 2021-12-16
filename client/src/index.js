42be3239990b91963b4af83fa76d0297037bc87a
Macbook <macbook@Macbook-MacBook-Pro.local>
2021-12-17 01:17:53 +0300

    place.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,7 @@ import Landing from "./components/Landing";
 import Attractions from "./components/Attractions";
 import Profile from "./components/Profile";
 import Forms from "./components/Forms";
-import Attractions from "./components/Attractions";
 import Contact from "./components/Contact";
 import About from "./components/About";
 
-ReactDOM.render(<Attractions />, document.getElementById("root"));
+ReactDOM.render(<Profile/>, document.getElementById("root"));
